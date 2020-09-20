b10551b966d82771d7bd512fdaa8fb08106a5275
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-20 18:39:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+require "pry"
+
+def line(katz_deli)
+
+end
+
+
+def take_a_number(katz_deli, names)
+  katz_delli = []
+    puts ("Welcome, #{name}. You are number #{katz_deli} in line.")
+
+
+end
