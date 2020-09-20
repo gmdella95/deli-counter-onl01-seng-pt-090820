65d515cc40b0a157d293cafd11637c7443c90bc7
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-20 18:39:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,19 @@
 require "pry"
 
 def line(katz_deli)
-
+  if katz_deli ==[]
+    puts "The line is currently empty."
+  else
+    new_array = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+    new_array<<" #{index + 1}. #{name}"
+    end
+    puts new_array
+  end
 end
 
-
 def take_a_number(katz_deli, names)
-  katz_delli = []
-    puts ("Welcome, #{name}. You are number #{katz_deli} in line.")
-
-
+  katz_deli.each_with_index do |name, index|
+  katz_deli<<names
+  end
 end
